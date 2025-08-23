1b7635f190020617f42be851ca08e97ea11ca1c5
jiyun921 <ryn0921@naver.com>
2025-08-24 00:35:21 +0900

    [chore]: application-test.yml 추가

--- src/test/java/com/ggamja/ggamja/GgamjaApplicationTests.java
+++ src/test/java/com/ggamja/ggamja/GgamjaApplicationTests.java
@@ -2,8 +2,10 @@ package com.ggamja.ggamja;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GgamjaApplicationTests {
 
 	@Test
