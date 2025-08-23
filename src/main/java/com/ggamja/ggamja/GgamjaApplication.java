5358488db0a537fe3e0fa4b157cae2c8debda968
jiyun921 <ryn0921@naver.com>
2025-08-23 21:37:22 +0900

    init

---
+++ src/main/java/com/ggamja/ggamja/GgamjaApplication.java
@@ -0,0 +1,13 @@
+package com.ggamja.ggamja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgamjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgamjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ggamja/ggamja/GgamjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggamja.ggamja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgamjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
