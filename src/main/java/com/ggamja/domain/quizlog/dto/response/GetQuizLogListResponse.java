56b9117da9465a3989b4171de12d9cf8441e75f9
jiyun921 <ryn0921@naver.com>
2025-08-28 16:19:43 +0900

    [feat]: GetQuizLogListResponse 구현

---
+++ src/main/java/com/ggamja/domain/quizlog/dto/response/GetQuizLogListResponse.java
@@ -0,0 +1,34 @@
+package com.ggamja.domain.quizlog.dto.response;
+
+import com.ggamja.domain.quizlog.entity.QuizLog;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record GetQuizLogListResponse(
+        @Schema(description = "퀴즈 로그 목록")
+        List<QuizLogDto> quizLogs,
+
+        @Schema(description = "다음 페이지 존재 여부", example = "true")
+        boolean hasNext
+) {
+    public static GetQuizLogListResponse of(List<QuizLogDto> quizLogs, boolean hasNext) {
+        return new GetQuizLogListResponse(quizLogs, hasNext);
+    }
+
+    public record QuizLogDto(
+            @Schema(description = "퀴즈 로그 ID", example = "1")
+            Long quizLogId,
+
+            @Schema(description = "퀴즈 푼 날짜", example = "2025-08-28T10:12:30")
+            LocalDateTime date
+    ) {
+        public static QuizLogDto from(QuizLog quizLog) {
+            return new QuizLogDto(
+                    quizLog.getId(),
+                    quizLog.getDate()
+            );
+        }
+    }
+}
