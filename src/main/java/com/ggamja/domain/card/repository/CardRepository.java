b14f7a5e13382a60bcfd7fd1bc8f0f0da862cee5
simchaehyeon <tlacogussla@konkuk.ac.kr>
2025-08-28 20:49:11 +0900

    [FEAT] 오늘의 카드 선정 스케줄러 구현

--- src/main/java/com/ggamja/domain/card/repository/CardRepository.java
+++ src/main/java/com/ggamja/domain/card/repository/CardRepository.java
@@ -1,7 +1,11 @@
 package com.ggamja.domain.card.repository;
 
 import com.ggamja.domain.card.entity.Card;
+import com.ggamja.domain.card.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CardRepository extends JpaRepository<Card, Long> {
+    List<Card> findByCategory(Category category);
 }
