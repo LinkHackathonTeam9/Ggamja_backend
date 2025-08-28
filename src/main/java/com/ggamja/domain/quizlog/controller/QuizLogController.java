b5dccba44c21b3f9d85dcc39a2acbb92aaa143d5
jiyun921 <ryn0921@naver.com>
2025-08-28 16:45:47 +0900

    [feat]: QuizLogController - getQuizLogDetail 구현

--- src/main/java/com/ggamja/domain/quizlog/controller/QuizLogController.java
+++ src/main/java/com/ggamja/domain/quizlog/controller/QuizLogController.java
@@ -1,11 +1,13 @@
 package com.ggamja.domain.quizlog.controller;
 
 import com.ggamja.domain.member.entity.Member;
+import com.ggamja.domain.quizlog.dto.response.GetQuizLogDetailResponse;
 import com.ggamja.domain.quizlog.dto.response.GetQuizLogListResponse;
 import com.ggamja.domain.quizlog.service.QuizLogService;
 import com.ggamja.global.docs.DocumentedApiErrors;
 import com.ggamja.global.response.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Sort;
@@ -13,6 +15,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +26,7 @@ import static com.ggamja.global.response.status.BaseExceptionResponseStatus.*;
 @RestController
 @RequestMapping("/api/quizlogs")
 @RequiredArgsConstructor
+@Tag(name = "QuizLog", description = "QuizLogController - 퀴즈 로그 관련 api")
 public class QuizLogController {
     private final QuizLogService quizLogService;
 
@@ -40,4 +44,18 @@ public class QuizLogController {
     ) {
         return ResponseEntity.ok(BaseResponse.ok(quizLogService.getQuizLogsList(member, pageable)));
     }
+
+    @Operation(
+            summary = "퀴즈 로그 상세 조회",
+            description = "특정 퀴즈 로그의 상세 정보를 조회합니다."
+    )
+    @DocumentedApiErrors({AUTH_UNAUTHENTICATED})
+    @GetMapping("/{id}")
+    public ResponseEntity<BaseResponse<GetQuizLogDetailResponse>> getQuizLogDetail(
+            @AuthenticationPrincipal Member member,
+            @PathVariable Long id
+    ) {
+        return ResponseEntity.ok(BaseResponse.ok(quizLogService.getQuizLogDetail(member, id)));
+    }
+
 }
