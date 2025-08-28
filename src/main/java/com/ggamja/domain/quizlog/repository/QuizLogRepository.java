566bb3c96885197b5fc28d8cb797a017d875160f
jiyun921 <ryn0921@naver.com>
2025-08-28 16:45:54 +0900

    [feat]: QuizLogService - getQuizLogDetail 구현

--- src/main/java/com/ggamja/domain/quizlog/repository/QuizLogRepository.java
+++ src/main/java/com/ggamja/domain/quizlog/repository/QuizLogRepository.java
@@ -8,8 +8,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface QuizLogRepository extends JpaRepository<QuizLog, Long> {
     Page<QuizLog> findByMember(Member member, Pageable pageable);
-
+    Optional<QuizLog> findByIdAndMember(Long id, Member member);
 }
--- src/main/java/com/ggamja/domain/quizlog/service/QuizLogService.java
+++ src/main/java/com/ggamja/domain/quizlog/service/QuizLogService.java
@@ -1,9 +1,11 @@
 package com.ggamja.domain.quizlog.service;
 
 import com.ggamja.domain.member.entity.Member;
+import com.ggamja.domain.quizlog.dto.response.GetQuizLogDetailResponse;
 import com.ggamja.domain.quizlog.dto.response.GetQuizLogListResponse;
 import com.ggamja.domain.quizlog.entity.QuizLog;
 import com.ggamja.domain.quizlog.repository.QuizLogRepository;
+import com.ggamja.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import org.springframework.data.domain.Pageable;
 import java.util.List;
+
+import static com.ggamja.global.response.status.BaseExceptionResponseStatus.*;
 
 @Service
 @RequiredArgsConstructor
@@ -26,5 +30,12 @@ public class QuizLogService {
                 .toList();
 
         return GetQuizLogListResponse.of(content, quizLogs.hasNext());
+    }
+
+    public GetQuizLogDetailResponse getQuizLogDetail(Member member, Long id) {
+        QuizLog quizLog = quizLogRepository.findByIdAndMember(id, member)
+                .orElseThrow(() -> new CustomException(QUIZLOG_NOT_FOUND));
+
+        return GetQuizLogDetailResponse.of(quizLog);
     }
 }
